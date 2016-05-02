bcd4393505168fab1ba8247c82cc6e6369d2a344
Darren Davies <ddavies@hotmail.co.uk>
2016-05-02 13:04:06 +0100

    Source tree initial test commit

---
+++ TestApplication1/TestApplication1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApplication1
+{
+    public class Calculator
+    {
+        public Func<int, int, int> Operation { get; set; }
+
+        public int Calculate(int lhs, int rhs)
+        {
+            return Operation(lhs, rhs);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            calc.Operation = (x, y) => { return x + y; };
+
+            Console.WriteLine(calc.Calculate(10, 20));
+            Console.Read();
+        }
+    }
+}
